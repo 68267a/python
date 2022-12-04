b2cc55f4bd63246f914900e7865acf073e733475
accountnumber3 <33899447+accountnumber3@users.noreply.github.com>
2022-12-04 15:01:51 -0500

    background and quit work

--- solitaire/solitaire.py
+++ solitaire/solitaire.py
@@ -8,30 +8,27 @@ doDebug = True
 screensize = 540,540
 width, height = screensize
 
-BLACK = (0, 0, 0)        # Background
-WHITE = (255,255,255)    # Labels
-GRAY = (127, 127, 127)   # Boxes
-YELLOW = (255, 255, 0)   # Board
-RED = (255, 0, 0)        # X
-BLUE = (0, 0, 255)       # O
-GREEN = (0, 255, 0)      # Win
-ORANGE = (255, 127, 0)   # CAT
+BLACK = (0, 0, 0)        # 
+WHITE = (255,255,255)    # 
+GRAY = (127, 127, 127)   # 
+YELLOW = (255, 255, 0)   # 
+RED = (255, 0, 0)        # 
+BLUE = (0, 0, 255)       # 
+GREEN = (0, 255, 0)      # 
+ORANGE = (255, 127, 0)   # 
+DARKGREEN = (24, 120, 74)#
+background = DARKGREEN
 
-cardsize = 64, 89, 16    # width, height, vertical offset
+cardsize = {"h":64, "w":89, "v":16} # width, height, vertical offset
+cardfont = (None, 12)
 cardvalues = {
-	"A": 1,
-	"2": 2,
-	"3": 3,
-	"4": 4,
-	"5": 5,
-	"6": 6,
-	"7": 7,
-	"8": 8,
-	"9": 9,
-	"10": 10,
-	"J": 11,
-	"Q": 12,
-	"K": 13
+	"A": 1,	"2": 2,	"3": 3,	"4": 4,	"5": 5,
+	"6": 6,	"7": 7,	"8": 8,	"9": 9,	"10": 10,
+	"J": 11,"Q": 12, "K": 13
+}
+cardsuits = {
+	"red": ("hearts","diamonds"),
+	"black": ("clubs","spades")
 }
 
 def debug(msg):
@@ -41,14 +38,15 @@ def main():
 	pygame.init()             # Let's go
 	screen = pygame.display.set_mode(screensize)
 	running = True
-	screen.fill(BLACK)
+	screen.fill(background)
 	debug("what now")
 	while running:
 		pygame.time.Clock().tick(15) # slow down
 		for event in pygame.event.get():
-			if event.type == pygame.quit:
+			if event.type == pygame.QUIT:
 				running = False
-
+			debug(event)
+		pygame.display.flip()
 	pygame.quit()
 
 
